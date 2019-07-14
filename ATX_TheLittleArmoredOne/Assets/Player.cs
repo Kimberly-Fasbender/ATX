2eca7b6e4c84f3a141208f918aa3f50fcca62f86
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-07-13 20:51:36 -0700

    added Player running and mirroring functionality

---
+++ ATX_TheLittleArmoredOne/Assets/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+
+    Rigidbody2D rigidBody;
+    Animator animator;
+    
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();  
+    }
+
+    void Update()
+    {
+        Run();
+        MirrorSprite();
+    }
+     private void Run()
+    {
+        float moveInput = Input.GetAxis("Horizontal"); 
+        Vector2 playerVelocity = new Vector2(moveInput * runSpeed, rigidBody.velocity.y);
+        rigidBody.velocity = playerVelocity;
+
+        // animation transition 
+        bool isRunning = Mathf.Abs(rigidBody.velocity.x) > 0;
+        animator.SetBool("Running", isRunning);
+    }
+
+     private void MirrorSprite()
+    {
+        bool isRunning = Mathf.Abs(rigidBody.velocity.x) > 0;
+
+        if (isRunning) 
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(rigidBody.velocity.x), 1f);
+        }
+    }
+}
