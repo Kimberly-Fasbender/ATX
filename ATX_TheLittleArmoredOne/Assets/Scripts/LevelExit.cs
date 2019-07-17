292a13e1574162028c36c3e8b719457b8dc25922
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-07-17 15:13:32 -0700

    added level exit functionality

--- ATX_TheLittleArmoredOne/Assets/Scripts/LevelExit.cs
+++ ATX_TheLittleArmoredOne/Assets/Scripts/LevelExit.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    float levelLoadDelay = 1.5f;
+    float slowMotionExit = 0.2f;
+
+    
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        StartCoroutine(LoadNextLevel());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadNextLevel()
     {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         
+        Time.timeScale = slowMotionExit;
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        Time.timeScale = 1f;
+        
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 }
