7b9d944abaf680879feaa46a8a9456be764ebe9e
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-07-26 13:48:37 -0700

    wrapping up level 2 design and added breakable blocks

---
+++ ATX_TheLittleArmoredOne/Assets/Scripts/BreakableTiles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableTiles : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Triggering");
+
+        Player player = FindObjectOfType<Player>();
+        Collider2D playerCollider = player.GetComponent<Collider2D>();
+
+
+        if (collision == playerCollider)
+        {
+            Debug.Log("destroying...");
+            Destroy(gameObject);
+        }
+    }
+}
