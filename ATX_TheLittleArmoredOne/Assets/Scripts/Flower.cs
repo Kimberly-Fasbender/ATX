53fee24f6a161aaa36134345dfed381437c8177e
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-07-27 19:58:59 -0700

    updated bouncy flower colliders and audio

---
+++ ATX_TheLittleArmoredOne/Assets/Scripts/Flower.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    [SerializeField] public AudioClip bounceSFX;
+    AudioListener audioListener;
+    Player player;
+
+    void Start()
+    {
+        audioListener = FindObjectOfType<AudioListener>();
+        player = FindObjectOfType<Player>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision == player.GetComponent<BoxCollider2D>())
+        {
+            AudioSource.PlayClipAtPoint(bounceSFX, audioListener.transform.position, 0.3f);
+        }
+    }
+}
